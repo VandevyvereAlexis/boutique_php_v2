f0eb76340f35943067457dfa0ce3315a3c0ee023
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2023-05-30 12:22:43 +0200

    structure de base

---
+++ produit.php
@@ -0,0 +1,22 @@
+<?php
+    // J'inclus le fichier des functions pour pouvoir les appeler ici
+    include 'functions.php';
+    session_start();
+    // J'inclus le head avec les balises de base + la balise head ( pour ne pas répéter le code qu'il contient )
+    include 'head.php';
+?>
+
+<body>
+    <?php
+        include 'header.php';
+    ?> 
+
+    <main>
+        <?php
+        ?>
+    </main>
+
+    <?php
+        include 'footer.php'
+    ?>
+</body>
